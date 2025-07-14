654a889f70b65b2eb1870d6ccfe6e2e9caff1133
Branden <bcrensh6@charlotte.edu>
2025-07-14 16:49:47 -0400

    Error Handling & Instructions

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # AI-Research-Assistant
-RAG Pipeline with a dataset of research papers used to help the user.
+
+RAG Pipeline with a dataset of research papers, textbooks, and more used to help the user.
+
+To use, simply use my link
+https://research-frontend-pi.vercel.app/
+
+The Grok api will stall if you request too quickly so wait a second before requesting again
+
+Previous queries are not saved meaning the model can't use it as context so be specific with your queries
+
+The database contains some ML/Systems/Calculus/Math so keep your questions in that range
+
+Or if youd like to host your own to make it more personalized
+
+Get your PINE api key from Pinecone
+Your Groq api key from
+and the TG api key from Together
+
+With this you can load the database on your own but some tips are:
+
+Make sure your embedding dimensions match your vector dimensions
+
+If hosting, do not host a model on your api backend, its too resource intensive
--- loadData.py
+++ loadData.py
@@ -6,18 +6,22 @@ from database import index
 from sentence_transformers import SentenceTransformer
 model = SentenceTransformer('all-mpnet-base-v2')
 def extractPDF(file: str, index) -> None: 
-    with open(file, 'rb') as pdf: 
-        reader = PyPDF2.PdfReader(pdf, strict=False)
-        pages = [] 
+    try: 
+        with open(file, 'rb') as pdf: 
+            reader = PyPDF2.PdfReader(pdf, strict=False)
+            pages = [] 
 
-        for i, page in enumerate(reader.pages):
-            try:
-                content = page.extract_text()
-                vec = model.encode(content).tolist()
-                id = str(uuid.uuid4())
-                index.upsert(vectors=[{"id": id, "values": vec, "metadata": {"content": content}}])
-            except Exception as e: 
-                print(f"Error on chunk {i}: {e}")
-                continue
+            for i, page in enumerate(reader.pages):
+                try:
+                    content = page.extract_text()
+                    vec = model.encode(content).tolist()
+                    id = str(uuid.uuid4())
+                    index.upsert(vectors=[{"id": id, "values": vec, "metadata": {"content": content}}])
+                except Exception as e: 
+                    print(f"Error on chunk {i}: {e}")
+                    continue
+        print("Done")
+    except Exception as e: 
+        print(e)
 
-extractPDF("./Data/Comp-Systems.pdf", index)           
+extractPDF("./Data/Diff-Eqn.pdf", index)           
